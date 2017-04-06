c6a16f85abb9ae071e17a80b5b8af3de0c9e3347
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-04-06 18:06:16 +0300

    Add UI test.

--- ToDoUITests/ToDoUITests.swift
+++ ToDoUITests/ToDoUITests.swift
@@ -9,15 +9,16 @@
 import XCTest
 
 class ToDoUITests: XCTestCase {
-        
+    
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+
+        app = XCUIApplication()
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
@@ -28,4 +29,34 @@ class ToDoUITests: XCTestCase {
         super.tearDown()
     }
     
+    func test_Test() {
+        
+        app.navigationBars["ToDo.ItemListView"].buttons["Add"].tap()
+        
+        let titleTextField = app.textFields["Title"]
+        titleTextField.tap()
+        titleTextField.typeText("123")
+        
+        let dateTextField = app.textFields["Date"]
+        dateTextField.tap()
+        dateTextField.typeText("02/22/2016")
+        
+        let locationTextField = app.textFields["Location"]
+        locationTextField.tap()
+        locationTextField.typeText("456")
+        
+        let addressTextField = app.textFields["Address"]
+        addressTextField.tap()
+        addressTextField.typeText("789")
+        
+        let descriptionTextField = app.textFields["Description"]
+        descriptionTextField.tap()
+        descriptionTextField.typeText("555")
+        app.buttons["Save"].tap()
+        
+        XCTAssertTrue(app.tables.staticTexts["123"].exists)
+        XCTAssertTrue(app.tables.staticTexts["02/22/2016"].exists)
+        XCTAssertTrue(app.tables.staticTexts["456"].exists)
+        
+    }
 }
